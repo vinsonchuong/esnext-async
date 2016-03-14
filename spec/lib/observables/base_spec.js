ab5bfffed9af714862544f63c5ef2c8b86d0dce4
Vinson Chuong <vinsonchuong@gmail.com>
2016-03-14 13:02:00 -0700

    Add simple observable transforms

--- spec/lib/observables/base_spec.js
+++ spec/lib/observables/base_spec.js
@@ -1,9 +1,33 @@
 import {Observable} from 'esnext-async';
 import {runTests as observableTests} from 'es-observable-tests';
 
+async function values(observable) {
+  const values = [];
+  await observable.forEach((value) => {
+    values.push(value);
+  });
+  return values;
+}
+
 describe('Observable', async () => {
   it('conforms to the es-observable spec', async () => {
     const {logger: {failed}} = await observableTests(Observable);
     expect(failed).toBe(0);
+  });
+
+  it('includes a map transform', async () => {
+    const observable = Observable.of(1, 2, 3).map((value) => value * 2);
+    expect(await values(observable)).toEqual([2, 4, 6]);
+  });
+
+  it('includes a filter transform', async () => {
+    const observable = Observable.of(1, 2, 3).filter((value) => value % 2 === 0);
+    expect(await values(observable)).toEqual([2]);
+  });
+
+  it('includes a flatMap transform', async () => {
+    const observable = Observable.of(1, 2, 3)
+      .flatMap((value) => [value, 2 * value]);
+    expect(await values(observable)).toEqual([1, 2, 2, 4, 3, 6]);
   });
 });
